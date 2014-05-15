82a59fc806418190576cdad591073bc29e12312e
fenix01 <p-yohann@hotmail.fr>
2014-05-15 14:32:27 +0200

    ajout d'un exemple pour le langage delphi

---
+++ example/delphi/sample.dpr
@@ -0,0 +1,15 @@
+program sample;
+
+uses
+  Vcl.Forms,
+  Main in 'Main.pas' {fmSample},
+  Connector in '..\..\wrapper\delphi\Connector.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfmSample, fmSample);
+  Application.Run;
+end.
