4120b9373198412be60ad7928ac124f6488f2d35
fenix01 <p-yohann@hotmail.fr>
2014-07-27 23:08:48 +0200

    updated the c++ wrapper to the new api
    this wrapper includes the new set of functions to fully control the
    memory scanner and the cheat table in c++

--- wrapper/c++/Connector.cpp
+++ wrapper/c++/Connector.cpp
@@ -2,14 +2,28 @@
 
 IGetProcessList iGetProcessList = 0;
 IOpenProcess iOpenProcess = 0;
-IResetScripts iResetScripts = 0;
-IAddScript iAddScript = 0;
-IRemoveScript iRemoveScript = 0;
-IActivateScript iActivateScript = 0;
 
-IProcessAddress iProcessAddress = 0;
+IResetTable iResetTable = 0;
+IAddScript iAddScript = 0;
+IRemoveRecord iRemoveRecord = 0;
+IActivateRecord iActivateRecord = 0;
+IApplyFreeze iApplyFreeze;
+
 IAddAddressManually iAddAddressManually = 0;
 IGetValue iGetValue = 0;
+ISetValue iSetValue = 0;
+IProcessAddress iProcessAddress = 0;
+
+IInitMemoryScanner iInitMemoryScanner = 0;
+INewScan iNewScan = 0;
+IConfigScanner iConfigScanner = 0;
+IFirstScan iFirstScan = 0;
+INextScan iNextScan = 0;
+ICountAddressesFound iCountAddressesFound = 0;
+IGetAddress iGetAddress = 0;
+IInitFoundList iInitFoundList = 0;
+IResetValues iResetValues = 0;
+IGetBinarySize iGetBinarySize = 0;
 
 
 
@@ -18,13 +32,28 @@ void loadFunctions(HINSTANCE libInst){
 	if (libInst){
 		iGetProcessList = (IGetProcessList)GetProcAddress(libInst, "IGetProcessList");
 		iOpenProcess = (IOpenProcess)GetProcAddress(libInst, "IOpenProcess");
-		iResetScripts = (IResetScripts)GetProcAddress(libInst, "IResetScripts");
-		iAddScript = (IAddScript)GetProcAddress(libInst, "IAddScript");
-		iRemoveScript = (IRemoveScript)GetProcAddress(libInst, "IRemoveScript");
-		iActivateScript = (IActivateScript)GetProcAddress(libInst, "IActivateScript");
 
-		iProcessAddress = (IProcessAddress)GetProcAddress(libInst, "IProcessAddress");
+		iResetTable = (IResetTable)GetProcAddress(libInst, "IResetTable");
+		iAddScript = (IAddScript)GetProcAddress(libInst, "IAddScript");
+		iRemoveRecord = (IRemoveRecord)GetProcAddress(libInst, "IRemoveRecord");
+		iActivateRecord = (IActivateRecord)GetProcAddress(libInst, "IActivateRecord");
+		iApplyFreeze = (IApplyFreeze)GetProcAddress(libInst, "IApplyFreeze");
+
+
 		iAddAddressManually = (IAddAddressManually)GetProcAddress(libInst, "IAddAddressManually");
 		iGetValue = (IGetValue)GetProcAddress(libInst, "IGetValue");
+		iSetValue = (ISetValue)GetProcAddress(libInst, "ISetValue");
+		iProcessAddress = (IProcessAddress)GetProcAddress(libInst, "IProcessAddress");
+		
+		iInitMemoryScanner = (IInitMemoryScanner)GetProcAddress(libInst, "IInitMemoryScanner");
+		iNewScan = (INewScan)GetProcAddress(libInst, "INewScan");
+		iConfigScanner = (IConfigScanner)GetProcAddress(libInst, "IConfigScanner");
+		iFirstScan = (IFirstScan)GetProcAddress(libInst, "IFirstScan");
+		iNextScan = (INextScan)GetProcAddress(libInst, "INextScan");
+		iCountAddressesFound = (ICountAddressesFound)GetProcAddress(libInst, "ICountAddressesFound");
+		iGetAddress = (IGetAddress)GetProcAddress(libInst, "IGetAddress");
+		iInitFoundList = (IInitFoundList)GetProcAddress(libInst, "IInitFoundList");
+		iResetValues = (IResetValues)GetProcAddress(libInst, "IResetValues");
+		iGetBinarySize = (IGetBinarySize)GetProcAddress(libInst, "IGetBinarySize");
 	}
 }
--- wrapper/c++/Connector.hpp
+++ wrapper/c++/Connector.hpp
@@ -2,48 +2,80 @@
 #define CONNECTOR_HPP
 #include <atlbase.h>
 
-enum VariableType {
-	vtByte = 0,
-	vtWord = 1,
-	vtDword = 2,
-	vtQword = 3,
-	vtSingle = 4,
-	vtDouble = 5,
-	vtString = 6,
-	vtUnicodeString = 7,
-	vtByteArray = 8,
-	vtBinary = 9,
-	vtAll = 10,
-	vtAutoAssembler = 11,
-	vtPointer = 12,
-	vtCustom = 13,
-	vtGrouped = 14,
-	vtByteArrays = 15
-}; //all ,grouped and MultiByteArray are special types
+enum TScanOption { soUnknownValue = 0, soExactValue = 1, soValueBetween = 2, soBiggerThan = 3, soSmallerThan = 4, soIncreasedValue = 5, soIncreasedValueBy = 6, soDecreasedValue = 7, soDecreasedValueBy = 8, soChanged = 9, soUnchanged = 10, soCustom };
+enum TScanType { stNewScan, stFirstScan, stNextScan };
+enum TRoundingType { rtRounded = 0, rtExtremerounded = 1, rtTruncated = 2 };
+enum TVariableType { vtByte = 0, vtWord = 1, vtDword = 2, vtQword = 3, vtSingle = 4, vtDouble = 5, vtString = 6, vtUnicodeString = 7, vtByteArray = 8, vtBinary = 9, vtAll = 10, vtAutoAssembler = 11, vtPointer = 12, vtCustom = 13, vtGrouped = 14, vtByteArrays = 15 }; //all ,grouped and MultiByteArray are special types
+enum TCustomScanType { cstNone, cstAutoAssembler, cstCPP, cstDLLFunction };
+enum TFastScanMethod { fsmNotAligned = 0, fsmAligned = 1, fsmLastDigits = 2 };
+enum Tscanregionpreference { scanDontCare, scanExclude, scanInclude };
 
 
 typedef void(__stdcall *IGetProcessList)(BSTR &processes);
 typedef void(__stdcall *IOpenProcess)(BSTR pid);
 
-typedef void(__stdcall *IResetScripts)();
+typedef void(__stdcall *IResetTable)();
 typedef void(__stdcall *IAddScript)(BSTR name, BSTR script);
-typedef void(__stdcall *IRemoveScript)(int id);
-typedef void(__stdcall *IActivateScript)(int id, bool activate);
+typedef void(__stdcall *IActivateRecord)(int id, bool activate);
+typedef void(__stdcall *IRemoveRecord)(int id);
+typedef void(__stdcall *IApplyFreeze)();
 
-typedef void(__stdcall *IProcessAddress)(BSTR address, VariableType vartype, bool showashexadecimal, bool showassigned , int bytesize, BSTR &res_address);
-typedef void(__stdcall *IAddAddressManually)(BSTR initialaddress, VariableType vartype);
+typedef void(__stdcall *IAddAddressManually)(BSTR initialaddress, TVariableType vartype);
 typedef void(__stdcall *IGetValue)(int id, BSTR &value);
+typedef void(__stdcall *ISetValue)(int id, BSTR value, bool freezer);
+typedef void(__stdcall *IProcessAddress)(BSTR address, TVariableType vartype, bool showashexadecimal, bool showassigned , int bytesize, BSTR &res_address);
+
+typedef void(__stdcall *IInitMemoryScanner)(HWND hwnd);
+typedef void(__stdcall *INewScan)();
+typedef void(__stdcall *IConfigScanner)(Tscanregionpreference scanWritable, Tscanregionpreference scanExecutable, Tscanregionpreference scanCopyOnWrite);
+
+typedef void(__stdcall *IFirstScan)(TScanOption scanOption, TVariableType variableType,
+	TRoundingType roundingtype, BSTR scanvalue1, BSTR scanvalue2,
+	BSTR startaddress, BSTR stopaddress, bool hexadecimal, bool binaryStringAsDecimal,
+	bool unicode, bool casesensitive, TFastScanMethod fastscanmethod,
+	BSTR fastscanparameter);
+
+typedef void(__stdcall *INextScan)(TScanOption scanOption, TRoundingType roundingtype, 
+	BSTR scanvalue1, BSTR scanvalue2, bool hexadecimal, bool binaryStringAsDecimal,
+	bool unicode, bool casesensitive, bool percentage, bool compareToSavedScan, 
+	BSTR savedscanname);
+
+typedef INT64(__stdcall *ICountAddressesFound)();
+typedef void(__stdcall *IGetAddress)(int index, BSTR &address, BSTR &value);
+typedef void(__stdcall *IInitFoundList)(TVariableType vartype, int varlength, 
+	bool hexadecimal, bool signed_, bool binaryasdecimal, bool unicode);
+typedef void(__stdcall *IResetValues)();
+typedef void(__stdcall *IRebaseAddressList)(int index);
+typedef void(__stdcall *IGetBinarySize)();
+
 
 extern IGetProcessList iGetProcessList;
 extern IOpenProcess iOpenProcess;
-extern IResetScripts iResetScripts;
-extern IAddScript iAddScript;
-extern IRemoveScript iRemoveScript;
-extern IActivateScript iActivateScript;
 
-extern IProcessAddress iProcessAddress;
+extern IResetTable iResetTable;
+extern IAddScript iAddScript;
+extern IActivateRecord iActivateRecord;
+extern IRemoveRecord iRemoveRecord;
+extern IApplyFreeze;
+
 extern IAddAddressManually iAddAddressManually;
 extern IGetValue iGetValue;
+extern ISetValue iSetValue;
+extern IProcessAddress iProcessAddress;
+
+extern IInitMemoryScanner iInitMemoryScanner;
+extern INewScan iNewScan;
+extern IConfigScanner iConfigScanner;
+extern IFirstScan iFirstScan;
+extern INextScan iNextScan;
+extern ICountAddressesFound iCountAddressesFound;
+extern IGetAddress iGetAddress;
+extern IInitFoundList iInitFoundList;
+extern IResetValues iResetValues;
+extern IRebaseAddressList iRebaseAddressList;
+extern IGetBinarySize iGetBinarySize;
+
+
 
 void loadFunctions(HINSTANCE libInst);
 
