fd0321ac3561f846971e739684f94a2e545aed75
fenix01 <p-yohann@hotmail.fr>
2014-07-27 22:47:40 +0200

    updated the delphi wrapper to the new api
    this wrapper includes the new set of functions to fully control the
    memory scanner and the cheat table in delphi

--- wrapper/delphi/Connector.pas
+++ wrapper/delphi/Connector.pas
@@ -12,23 +12,27 @@ type TRoundingType=(rtRounded=0,rtExtremerounded=1,rtTruncated=2);
 type TVariableType=(vtByte=0, vtWord=1, vtDword=2, vtQword=3, vtSingle=4, vtDouble=5, vtString=6, vtUnicodeString=7, vtByteArray=8, vtBinary=9, vtAll=10, vtAutoAssembler=11, vtPointer=12, vtCustom=13, vtGrouped=14, vtByteArrays=15); //all ,grouped and MultiByteArray are special types
 type TCustomScanType=(cstNone, cstAutoAssembler, cstCPP, cstDLLFunction);
 type TFastScanMethod=(fsmNotAligned=0, fsmAligned=1, fsmLastDigits=2);
+type Tscanregionpreference=(scanDontCare, scanExclude, scanInclude);
 
 type
   TGetProcessList = procedure(out processes : WideString); stdcall;
   TOpenProcess = procedure(pid: WideString); stdcall;
 
-  TResetScripts = procedure(); stdcall;
+  TResetTable = procedure(); stdcall;
   TAddScript = procedure(name: WideString; script: WideString); stdcall;
-  TRemoveScript = procedure(id: integer); stdcall;
-  TActivateScript = procedure(id: integer; activate: boolean); stdcall;
+  TActivateRecord = procedure(id: integer; activate: boolean); stdcall;
+  TRemoveRecord = procedure(id: integer); stdcall;
 
+  TAddAddressManually = procedure (initialaddress: WideString=''; vartype: TVariableType=vtDword);stdcall;
+  TGetValue = procedure(id : integer ; out value : WideString);stdcall;
+  TSetValue = procedure(id : integer ; value : WideString ; freezer : boolean);stdcall;
   TProcessAddress = procedure(address : WideString ; vartype : TVariableType ; showashexadecimal: boolean;
   showAsSigned: boolean; bytesize: Integer; out pvalue : WideString);stdcall;
-  TAddAddressManually = procedure (initialaddress: WideString=''; vartype: TVariableType=vtDword);stdcall;
-  TGetValue = procedure (id : integer ; out value : WideString);stdcall;
 
-  TInitMemoryScanner = procedure();stdcall;
-  TDeinitMemoryScanner = procedure();stdcall;
+  TInitMemoryScanner = procedure(hwnd : THandle);stdcall;
+  TNewScan = procedure();stdcall;
+  TConfigScanner = procedure (scanWritable: Tscanregionpreference;
+  scanExecutable: Tscanregionpreference;scanCopyOnWrite: Tscanregionpreference);stdcall;
 
   TFirstScan = procedure(scanOption: TScanOption; VariableType: TVariableType;
   roundingtype: TRoundingType; scanvalue1, scanvalue2: WideString; startaddress,stopaddress: WideString;
@@ -36,35 +40,42 @@ type
   fastscanparameter: WideString=''); stdcall;
 
   TNextScan = procedure (scanOption: TScanOption; roundingtype: TRoundingType;
-  scanvalue1, scanvalue2: string;
+  scanvalue1, scanvalue2: WideString;
   hexadecimal,binaryStringAsDecimal, unicode, casesensitive,percentage,compareToSavedScan: boolean;
-  savedscanname: string); stdcall;
+  savedscanname: WideString); stdcall;
 
   TCountAddressesFound = function():Int64;stdcall;
-  TRegisterScanDoneCallback = procedure(fun : TOnScanDoneCallback);stdcall;
-  TInitializeFoundList = procedure();stdcall;
   TGetAddress = procedure(index : integer; out address : WideString ; out value : WideString);
+  TInitFoundList = procedure (vartype: TVariableType; varlength: integer; hexadecimal,
+  signed,binaryasdecimal,unicode: boolean);stdcall;
+  TResetValues = procedure();stdcall;
+  TRebaseAddressList = procedure(index : integer);stdcall;
+  TGetBinarySize = function():Int64;stdcall;
 
 var
   IGetProcessList: TGetProcessList;
   IOpenProcess: TOpenProcess;
-  IResetScripts: TResetScripts;
+  IResetTable: TResetTable;
   IAddScript: TAddScript;
-  IRemoveScript: TRemoveScript;
-  IActivateScript: TActivateScript;
+  IRemoveRecord: TRemoveRecord;
+  IActivateRecord: TActivateRecord;
 
   IProcessAddress : TProcessAddress;
   IAddAddressManually : TAddAddressManually;
   IGetValue : TGetValue;
+  ISetValue : TSetValue;
 
   IInitMemoryScanner : TInitMemoryScanner;
-  IDeinitMemoryScanner : TDeinitMemoryScanner;
+  IConfigScanner : TConfigScanner;
+  INewScan : TNewScan;
   IFirstScan : TFirstScan;
   INextScan : TNextScan;
   ICountAddressesFound : TCountAddressesFound;
-  IRegisterScanDoneCallback : TRegisterScanDoneCallback;
-  IInitializeFoundList : TInitializeFoundList;
   IGetAddress : TGetAddress;
+  IInitFoundList : TInitFoundList;
+  IResetValues : TResetValues;
+  IRebaseAddressList : TRebaseAddressList;
+  IGetBinarySize : TGetBinarySize;
 
 procedure loadFunctions(handle: THandle);
 
@@ -74,23 +85,28 @@ procedure loadFunctions(handle: THandle);
 begin
   @IGetProcessList := GetProcAddress(handle, 'IGetProcessList');
   @IOpenProcess := GetProcAddress(handle, 'IOpenProcess');
-  @IResetScripts := GetProcAddress(handle, 'IResetScripts');
-  @IAddScript := GetProcAddress(handle, 'IAddScript');
-  @IRemoveScript := GetProcAddress(handle, 'IRemoveScript');
-  @IActivateScript := GetProcAddress(handle, 'IActivateScript');
 
-  @IProcessAddress := GetProcAddress(handle, 'IProcessAddress');
+  @IResetTable := GetProcAddress(handle, 'IResetTable');
+  @IAddScript := GetProcAddress(handle, 'IAddScript');
+  @IActivateRecord := GetProcAddress(handle, 'IActivateScript');
+  @IRemoveRecord := GetProcAddress(handle, 'IRemoveScript');
+
   @IAddAddressManually := GetProcAddress(handle, 'IAddAddressManually');
   @IGetValue := GetProcAddress(handle, 'IGetValue');
+  @ISetValue := GetProcAddress(handle, 'ISetValue');
+  @IProcessAddress := GetProcAddress(handle, 'IProcessAddress');
 
   @IInitMemoryScanner := GetProcAddress(handle, 'IInitMemoryScanner');
-  @IDeinitMemoryScanner := GetProcAddress(handle, 'IDeinitMemoryScanner');
+  @INewScan := GetProcAddress(handle, 'INewScan');
+  @IConfigScanner := GetProcAddress(handle, 'IConfigScanner');
   @IFirstScan := GetProcAddress(handle, 'IFirstScan');
   @INextScan := GetProcAddress(handle, 'INextScan');
   @ICountAddressesFound := GetProcAddress(handle, 'ICountAddressesFound');
-  @IRegisterScanDoneCallback := GetProcAddress(handle, 'IRegisterScanDoneCallback');
-  @IInitializeFoundList := GetProcAddress(handle, 'IInitializeFoundList');
   @IGetAddress := GetProcAddress(handle, 'IGetAddress');
+  @IInitFoundList := GetProcAddress(handle, 'IInitFoundList');
+  @IResetValues := GetProcAddress(handle, 'IResetValues');
+  @IRebaseAddressList := GetProcAddress(handle, 'IRebaseAddressList');
+  @IGetBinarySize := GetProcAddress(handle, 'IGetBinarySize');
 end;
 
 end.
